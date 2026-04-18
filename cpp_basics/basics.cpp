a5099bf4b23dd91c8494dd2447ee3d2436c4d24e
Purbasha Goswami <purbashagoswami2@gmail.com>
2026-04-18 17:38:46 +0530

    first commit

---
+++ cpp_basics/basics.cpp
@@ -0,0 +1,138 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // user input/output
+
+    // cin - used for taking input from user
+    // cout - used for printing output
+    // endl - endline/used for next line
+    // \n - also used for next line
+
+    // int x,y;
+    // cin>>x>>y;
+    // cout<<"The value of x: "<<x<<endl<<"The value of y: "<<y;
+
+    // data types
+
+    // int
+
+    // int x;
+    // cin>>x;
+    // cout<<"the number is: "<<x;
+
+    // string
+    // strings takes more space than char and it is declared within "double quote"
+
+    // string s1,s2;
+    // cin>>s1>>s2;
+    // cout<<s1<<s2;
+
+    // getline - it displays the string written in a single line. It does not pick up the string from next line or after endl.
+
+    // string s;
+    // getline(cin,s);
+    // cout<<s;
+
+    // string s1="Purbasha Goswami";
+    // cout<<s1;
+
+    // char
+    // char takes less space and it is declared within 'single quote'. It is used to declare single characters
+
+
+    // if-else statements
+
+    // int age;
+    // cout<<"Enter your age"<<endl;
+    // cin>>age;
+    // if(age>=18){
+    //     cout<<"Adult";
+    // }else{
+    //     cout<<"Minor";
+    // }
+
+    // else-if statements
+
+    // int marks;
+    // cin>>marks;
+
+    // if(marks<=25){
+    //     cout<<"F";
+    // }else if(marks<=50){
+    //     cout<<"E";
+    // }else if(marks<=70){
+    //     cout<<"O";
+    // }
+
+    // Arrays
+
+    // Arrays are stored consecutively in memory. The 1st element of array can be stored in random memory address but the following will store consecutively.
+
+    // Undefined Array element values holds garbage value.
+
+    // 1D Array
+
+    // int arr[5];
+    // cin>>arr[0]>>arr[1]>>arr[2]>>arr[3]>>arr[4];
+
+    // // Modifying an array integer
+    // arr[3]+=10;
+    // cout<<arr[3];
+
+    // 2D Array
+
+    // int arr[3][5];
+
+    // arr[1][1]=90;
+    // cout<<arr[1][1]<<endl;
+    // cout<<arr[2][4];
+
+    // for loops
+
+    // for ( int i = 0; i <= 5; i++)
+    // {
+    //     cout<<"Learning DSA"<<endl;
+    // }
+
+    // while loops
+
+    // int i=1;
+
+    // while(i<=5){
+    //     cout<<"learned while loop"<<endl;
+    //     i++;
+    // }
+
+    // do-while loops
+
+    // int i=1;
+    // do{
+    //     cout<<"Learning do-while loop"<<endl;
+    //     i++;
+    // }while(i<=5);
+
+    // Runs code atleast once even if the condition is wrong
+
+    // do{
+    //     cout<<"Learned do while"<<endl;
+    //     i++;
+    // }while(i>=5);
+
+    // Functions
+
+    // Types of functions
+    // void
+    // parameterised
+    // non-parameterised
+    // return
+
+    // Pass by value
+
+    
+
+    
+    
+
+    return 0;
+}
---
+++ pattern_printing/pattern7.cpp
@@ -0,0 +1,19 @@
+// 1 2 3 4
+// 2 3 4
+// 3 4 
+// 4
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n=4;
+    for(int i=1;i<=n;i++){
+        for(int j=i;j<=n;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
