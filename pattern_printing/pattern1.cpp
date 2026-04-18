fbc59dd27d3c48002d87bafb8f5830c29e7b2f7b
Purbasha Goswami <purbashagoswami2@gmail.com>
2026-04-18 22:28:59 +0530

    solved 8 pattern printing

--- pattern_printing/pattern1.cpp
+++ pattern_printing/pattern1.cpp
@@ -7,10 +7,9 @@
 using namespace std;
 
 int main(){
-    int n=3;
     char ch='A';
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
+    for(int i=1;i<=3;i++){
+        for(int j=1;j<=3;j++){
             cout<<ch<<" ";
             ch++;
         }
--- pattern_printing/pattern2.cpp
+++ pattern_printing/pattern2.cpp
@@ -8,9 +8,9 @@
 using namespace std;
 
 int main(){
-    int n=4;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<i+1;j++){
+
+    for(int i=0;i<4;i++){
+        for(int j=0;j<=i;j++){
             cout<<"* ";
         }
         cout<<endl;
